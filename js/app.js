b3a495b6f1d8a020887849d8aedc75c70122b25a
atclarkson <atclarkson@gmail.com>
2018-06-25 09:29:36 -0400

    :sparkles: add click functionality to cat images

--- js/app.js
+++ js/app.js
@@ -11,37 +11,61 @@ class CatClicker {
     this.catPic = document.getElementById(`catPic-${this.index}`);
   }
   addOne() {
-    catPic.addEventListener('click', function(){
-      this.counter += 21;
-      this.clickCounter.textContent=counter;
+    let self = this;
+    this.catPic.addEventListener('click', function(){
+      self.counter += 21;
+      self.clickCounter.textContent = self.counter;
     }, false);
-    clickCounter.addEventListener('click', function(){
-      this.counter++;
-      this.clickCounter.textContent=counter;
+    this.clickCounter.addEventListener('click', function(){
+      self.counter++;
+      self.clickCounter.textContent = self.counter;
     }, false);
   }
 }
 
+const catPicArray = [
+  'img/cat-2083492_1920.jpg',
+  'img/cat-1285634_1920.png',
+  'img/cat-323262_1920.jpg',
+  'img/animal-339400_1920.jpg'
+];
+
+let cats = [];
+const numCats = 4;
+for (let i = 0; i < numCats; i++) {
+  cats.push(new CatClicker(catPicArray[i], i));
+}
+
 // Build the dom.  Run through an array of catClicker objects and assemble the dom
-
-
+const containerDiv = document.querySelector('.flex-container');
+for (let cat of cats) {
+  containerDiv.innerHTML +=
+    `
+    <div class=\"container\">
+      <img id=\"catPic-${cat.index}\" src=${catPicArray[cat.index]} alt=\"A cat image to be clicked on\">
+      <div id=\"numClicks-${cat.index}\" class=\"counter noselect\">0</div>
+    </div>
+    `
+}
 
 // Get Dom Elements and Add an event listener to each objects
-
-
+for (let cat of cats) {
+  cat.getDomElements();
+  cat.addOne();
+}
 //
 
 
 
 
-let counter = 0;
-catPic.addEventListener('click', function(){
-  counter += 21;
-  console.log(counter);
-  clickCounter.textContent=counter;
-}, false);
-clickCounter.addEventListener('click', function(){
-  counter++;
-  console.log(counter);
-  clickCounter.textContent=counter;
-}, false);
+// let counter = 0;
+// catPic.addEventListener('click', function(){
+//   counter += 21;
+//   console.log(counter);
+//   clickCounter.textContent=counter;
+// }, false);
+// clickCounter.addEventListener('click', function(){
+//   counter++;
+//   console.log(counter);
+//   clickCounter.textContent=counter;
+// }, false);
